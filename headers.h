7112c04510908005b4c47baca1cd085d29451c9f
rafael <rafael.19.rosendo@gmail.com>
2024-06-06 21:22:52 -0300

    att

--- headers.h
+++ headers.h
@@ -5,12 +5,12 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>  
-#include <SDL2/SDL.h> 
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h> 
+//#include <glm/glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>  
+//#include <SDL2/SDL.h> 
+//#include <SDL2/SDL_image.h>
+//#include <SDL2/SDL_ttf.h>
+//#include <SDL2/SDL_mixer.h> 
 #include<iostream>
 #include<fstream>
 #include<sstream>
